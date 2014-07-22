68c2b981fe772be4f05a1177bf6e4cc2270659ce
nghia nguyen <Nghia@EvilGeniusTechnologies.com>
2014-07-22 16:08:18 +0700

    nghia commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io =  require('socket.io')(http);
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
+app.get('/', function(req, res){
+    res.sendfile('index.html');
 });
 
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
 });
