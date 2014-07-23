766a8ea40db835b6acf6c80a5b196816a4672b66
Huy, Vuong Do Thanh <vuongdothanhhuy@gmail.com>
2014-07-23 10:56:23 +0700

    rest api

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
-var express = require('express');
-var app = express();
+// BASE SETUP
+// =============================================================================
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser());
+
+var port = process.env.PORT || 2000;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
 });
 
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+// test route to make sure everything is working (accessed at GET http://localhost:2000/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
 });
+
+// more routes for our API will happen here
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:2000/api/bears)
+    .post(function(req, res) {
+        res.json({ message: 'Bear created!' });
+    })
+    // get all the bears (accessed at GET http://localhost:2000/api/bears)
+    .get(function(req, res) {
+        res.json({ message: 'Bear here!' });
+    });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
