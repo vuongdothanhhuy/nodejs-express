c5fb8576c77e2462ab9763cb0545c2371d48278c
nghia nguyen <Nghia@EvilGeniusTechnologies.com>
2014-08-12 09:24:18 +0700

    nghia

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io =  require('socket.io')(http);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('inComingMessage', function(message){
-        io.emit('inComming', message);
-    });
+app.get('/sample', function (req, res) {
+    res.send('this is a sample!');
 });
 
-http.listen(3000, function(){
+app.post('/example', function(req, res){
+    res.send('Username: ' + req.body.username);
+});
+
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
+
+//io.on('connection', function (socket) {
+//    socket.on('inComingMessage', function (message) {
+//        var censor = 'fuck';
+//        var rep = message.replace(censor, '*beep*');
+//        io.emit('inComming', rep);
+//    });
+//});
